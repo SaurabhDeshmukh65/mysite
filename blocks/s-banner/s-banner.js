aed012b298c7db8913b73feb5fb8747ac3cf0262
Deshmukh <2358396@cognizant.com>
2025-05-15 17:33:23 +0530

    first change

--- blocks/s-banner/s-banner.js
+++ blocks/s-banner/s-banner.js
@@ -1,22 +1,24 @@
 export default function decorate(block) {
   const rows = Array.from(block.children);
-  block.classList.add('s-banner');
 
   rows.forEach((row, rowIndex) => {
-    if (rowIndex === 0) {
-      row.classList.add('s-banner-header');
-    } else if (rowIndex === 1) {
-      row.classList.add('s-banner-content');
-      const columns = Array.from(row.children);
-      columns.forEach((col, colIndex) => {
+    const columns = Array.from(row.children);
+    columns.forEach((col, colIndex) => {
+      if (rowIndex === 0) {
         if (colIndex === 0) {
           col.classList.add('s-banner-title');
+          col.querySelector('h1').classList.add('s-banner-title-text');
         } else if (colIndex === 1) {
           col.classList.add('s-banner-subtitle');
+          col.querySelector('p').classList.add('s-banner-subtitle-text');
         }
-      });
-    } else if (rowIndex === 2) {
-      row.classList.add('s-banner-image');
-    }
+      } else if (rowIndex === 1) {
+        col.classList.add('s-banner-image');
+        const pic = col.querySelector('picture');
+        if (pic) {
+          pic.classList.add('s-banner-image-content');
+        }
+      }
+    });
   });
 }
