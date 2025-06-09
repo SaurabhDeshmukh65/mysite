457196a007ce623048fc2b2763b0ffe6d7b32dc1
snehal-Tele <snehal.tele@cognizant.com>
2025-06-09 16:45:06 +0530

    m4

--- blocks/s-quarter/s-quarter.js
+++ blocks/s-quarter/s-quarter.js
@@ -1,34 +1,12 @@
 export default function decorate(block) {
-    block.classList.add('s-quarter-block');
-  
-    const columns = Array.from(block.children);
-    columns.forEach((col) => {
-      col.classList.add('s-quarter-col');
-  
-      const [number, label] = col.children;
-      if (number) number.classList.add('s-quarter-number');
-      if (label) label.classList.add('s-quarter-label');
-    });
-  }
+  block.classList.add('s-quarter-block');
 
-  
-// export default function decorate(block) {
-//   block.classList.add('s-quarter-block');
- 
-//   const columns = Array.from(block.children);
- 
-//   columns.forEach((col, index) => {
-//     col.classList.add('s-quarter-col');
- 
-//     // Check if it's the last column
-//     const isLast = index === columns.length - 1;
-//     const [number, label] = col.children;
- 
-//     if (isLast) {
-//       col.classList.add('s-quarter-button'); //Add class for button styling
-//     } else {
-//       if (number) number.classList.add('s-quarter-number');
-//       if (label) label.classList.add('s-quarter-label');
-//     }
-//   });
-// }
+  const columns = Array.from(block.children);
+  columns.forEach((col) => {
+    col.classList.add('s-quarter-col');
+
+    const [number, label] = col.children;
+    if (number) number.classList.add('s-quarter-number');
+    if (label) label.classList.add('s-quarter-label');
+  });
+}
